f1155683463dfeae8c6defdb7c75d02da52aebb2
ameerqatmosh <ameerqatmosh@gmail.com>
2026-05-19 10:49:10 +0300

    fix-main.tsx-url-issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { HashRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <App />
-  </StrictMode>
+  // <StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
+  // </StrictMode>
 )
